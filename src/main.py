b3b5b7eaa60b51e85879e6eade0dec4f257236ef
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-01 05:47:55 +0530

    feat(main): User commands dictionary with option for exiting and barebons 'Inventory' option

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 print("=" * 28)
 print("Welcome to Printos accounting!")
 print("=" * 28)
+
+user_commands = {
+    "1": {
+        "command": "Inventory",
+        "options": {
+            "1": "Add product",
+            "2": "List product",
+            "0": "Return",
+        },
+    },
+    "0": {"command": "exit"},
+}
