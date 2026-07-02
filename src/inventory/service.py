0f365e1fef974accdc8bca80d701826a664d4e67
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-02 06:20:08 +0530

    feature(inventory): Basic options displaying for inventory and break logic

---
+++ src/inventory/service.py
@@ -0,0 +1,21 @@
+options = {
+    "1": "Add product",
+    "2": "Remove product",
+}
+
+
+def start():
+    while True:
+        for option, description in options.items():
+            print(f"{option}: {description}")
+
+        print("0: Return")
+
+        choice = input("Choice: ")
+
+        if choice == 0:
+            return
+
+        if choice not in options.keys():
+            print("Invalid option.")
+            continue
