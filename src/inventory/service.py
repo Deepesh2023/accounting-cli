a08c2feeed1ed64d0e655e52e3212c818680af3e
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-03 06:57:28 +0530

    feat(inventory): Basic functionality for adding a product in 'inventory/service.py'

--- src/inventory/service.py
+++ src/inventory/service.py
@@ -1,3 +1,5 @@
+from inventory.models import Product
+
 MENU_OPTIONS = {
     "1": "Add product",
     "2": "Remove product",
@@ -17,3 +19,17 @@ def show_menu():
         if choice not in MENU_OPTIONS.keys():
             print("Invalid option.")
             continue
+
+        if choice == "1":
+            add_product()
+
+
+def add_product():
+    print("Add product")
+
+    name = input("Name: ")
+    selling_price = input("Selling price: ")
+    qnt = input("Quantity: ")
+
+    product = Product(id="1", name=name, selling_price=selling_price, quantity=qnt)
+    print(product)
