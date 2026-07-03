9ba93c5af1ab22016a3d9a6856628837fc131973
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-03 06:17:15 +0530

    refactor(cli): User constant variable format for 'user_commands' dict in 'cli/menu.py'

--- src/cli/menu.py
+++ src/cli/menu.py
@@ -1,18 +1,18 @@
 from inventory.service import show_menu as show_inventory_menu
 
+USER_COMMANDS = {
+    "1": "Inventory",
+    "0": "exit",
+}
+
 
 def show_main_menu():
     print("=" * 28)
     print("Welcome to Printos accounting!")
     print("=" * 28)
 
-    user_commands = {
-        "1": "Inventory",
-        "0": "exit",
-    }
-
     while True:
-        for command_key, command in user_commands.items():
+        for command_key, command in USER_COMMANDS.items():
             print(f"{command_key}: {command}")
             print()
 
@@ -21,7 +21,7 @@ def show_main_menu():
         if choice == "0":
             break
 
-        if choice not in user_commands.keys():
+        if choice not in USER_COMMANDS.keys():
             print("Invalid command.")
             continue
 
