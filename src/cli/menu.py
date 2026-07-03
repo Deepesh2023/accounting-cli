1c47411c179edd0b6b5ccf390fe6d1cfb4d95e48
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-03 05:35:44 +0530

    chore(cli): Moved all code from 'main' to 'src/cli/menu.py'

---
+++ src/cli/menu.py
@@ -0,0 +1,31 @@
+from inventory.service import start as inventory_start
+
+print("=" * 28)
+print("Welcome to Printos accounting!")
+print("=" * 28)
+
+user_commands = {
+    "1": "Inventory",
+    "0": "exit",
+}
+
+while True:
+    for command_key, command in user_commands.items():
+        print(f"{command_key}: {command}")
+        print()
+
+    choice = input("Choice: ")
+
+    if choice == "0":
+        break
+
+    if choice not in user_commands.keys():
+        print("Invalid command.")
+        continue
+
+    if choice == "1":
+        inventory_start()
+
+print("=" * 28)
+print("Goodbye!")
+print("=" * 28)
