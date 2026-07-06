1596e6a4df579f8046826a4ce1dd3ccb9730e413
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-06 10:08:02 +0530

    feat(inventory/repository): Inventory reposotiry can now list products with an optional filter of archived

--- src/inventory/repository.py
+++ src/inventory/repository.py
@@ -10,5 +10,11 @@ class _Inventory:
     def add_product(self, product: Product):
         self._products.append(product)
 
+    def list_products(self, show_archived=False):
+        if show_archived:
+            return self._products
+
+        return [product for product in self._products if not product.archived]
+
 
 inventory = _Inventory()
