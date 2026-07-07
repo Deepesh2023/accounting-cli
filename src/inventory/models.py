7ee18321091968055f990b47e16415a16389f29f
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-07 07:40:50 +0530

    refactor(inventory/models): renamed 'id' property of 'Product' to 'product_id'

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from uuid import UUID
 
+
 @dataclass
 class Product:
-    id: UUID
+    product_id: UUID
     name: str
     selling_price: float
     quantity: int
