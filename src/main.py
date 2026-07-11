b91084b8811cc6da179599710e1160d53e597e20
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-11 07:34:04 +0530

    Initialize SaleRepository in main entry point

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from cli.menu import show_main_menu
 from inventory.repository import InventoryRepository
+from sale.repository import SaleRepository
 
 inventory_repository = InventoryRepository()
+sale_repository = SaleRepository()
 
-show_main_menu(inventory_repository=inventory_repository)
+show_main_menu(inventory_repository=inventory_repository, sale_repository=sale_repository)
