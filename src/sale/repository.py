0c7ddcd3e8ef892bb0105f753563348c517a14d9
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-11 09:33:18 +0530

    Implement SaleRepository._save_to_disk

--- src/sale/repository.py
+++ src/sale/repository.py
@@ -13,7 +13,25 @@ class SaleRepository:
         return []
 
     def _save_to_disk(self):
-        pass
+        data = [
+            {
+                "sale_id": str(s.sale_id),
+                "date": s.date.isoformat(),
+                "customer_name": s.customer_name,
+                "items": [
+                    {
+                        "product_id": str(si.product_id),
+                        "name": si.name,
+                        "selling_price": si.selling_price,
+                        "quantity": si.quantity
+                    }
+                    for si in s.items
+                ]
+            }
+            for s in self._sales
+        ]
+        with open(self._storage_file, "w") as f:
+            json.dump(data, f, indent=4)
 
     def add_sale(self, sale: Sale):
         self._sales.append(sale)
