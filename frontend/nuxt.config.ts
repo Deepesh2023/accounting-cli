13c8c3c8a0654a26587f79fdbb8ebf5907967083
Deepesh2023 <officialdeepeshsg@gmail.com>
2026-07-23 07:03:16 +0530

    delete frontend

--- frontend/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon', '@pinia/nuxt'],
-  components: [
-    { path: '~/components', pathPrefix: false },
-  ],
-})
